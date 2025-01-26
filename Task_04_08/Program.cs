afe44b9255b69b4e4fd4b1091138e220cd84b5e3
BYDDA0 <renatyahin49@gmail.com>
2025-01-26 23:00:14 +0500

    Выполнено 8 задание 4 урока

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {/*Дан массив, содержащий последовательность 50 случайных чисел. Найти количество пар элементов, значения
+        которых равны.*/
+        static void Main(string[] args)
+        {
+            int[] number = new int[50];
+            Random rnd = new Random();
+            int pary = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = rnd.Next(1, 11);
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    if (number[i] == number[j])
+                    {
+                        pary++;
+                    }
+                }
+
+            }
+            Console.WriteLine($"Количество пар чисел - {pary}");
+            
+        }
+    }
+}
