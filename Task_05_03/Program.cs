9d29cd8c4987d167ba463181b0aa5548b0e0ad44
BYDDA0 <renatyahin49@gmail.com>
2025-02-01 23:37:22 +0500

    Выполнено 3 задача 5 урока

--- Task_05_03/Program.cs
+++ Task_05_03/Program.cs
@@ -2,82 +2,112 @@
 {
     internal class Program
     {
-       /* Даны два массива, заполненные символами английского алфавита размером 3*3.
-        * Проверить, являются ли матрицы равными, если
-        * да, то вывести сообщение о том, что они равны, если нет,
-        * то вывести повторно матрицы с цветовой индикацией только тех
-        * элементов, которые равны
-       . (матрицы считаются равными, если их соответствующие элементы равны. */
-        static void Main(string[] args)
+        /* Даны два массива, заполненные символами английского алфавита размером 3*3.
+         * Проверить, являются ли матрицы равными, если
+         * да, то вывести сообщение о том, что они равны, если нет,
+         * то вывести повторно матрицы с цветовой индикацией только тех
+         * элементов, которые равны
+        . (матрицы считаются равными, если их соответствующие элементы равны. */
+        static void Main()
         {
-            char[,] matritsa1 = {
-            { 'f', 'h', 'h' },
-            { 'f', 'c', 'h' },
-            { 'w', 'g', 'k' }
-        };
+            // Создаем две матрицы размером 3x3 
+            char[,] matritsa1 = new char[3, 3];
+            char[,] matritsa2 = new char[3, 3];
 
-            char[,] matritsa2 = {
-            { 'f', 'h', 'h' },
-            { 'f', 'c', 'x' },
-            { 'a', 'f', 'j' }
-        };
-
-            bool bi = CompareMatrices(matritsa1, matritsa2);
-
-            if (bi)
+            // Заполняем первую матрицу
+            Console.WriteLine("Введите элементы первой матрицы (3x3):");
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Матрицы равны");
+                for (int j = 0; j < 3; j++)
+                {
+                    matritsa1[i, j] = Console.ReadKey().KeyChar;
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine("Матрицы не равны");
-                DisplayMatrices(matritsa1, matritsa2);
-            }
-        }
 
-        static bool CompareMatrices(char[,] matritsa1, char[,] matritsa2)
-        {
+            // Заполняем вторую матрицу
+            Console.WriteLine("\nВведите элементы второй матрицы (3x3):");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matritsa2[i, j] = Console.ReadKey().KeyChar;
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            // Проверяем, равны ли они
+            bool areEqual = true;
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     if (matritsa1[i, j] != matritsa2[i, j])
                     {
-                        return false;
+                        areEqual = false;
+                        break;
                     }
                 }
+                if (!areEqual) break;
             }
-            return true;
-        }
 
-        static void DisplayMatrices(char[,] matritsa1, char[,] matritsa2)
-        {
-            Console.WriteLine("Матрицы:");
-            for (int i = 0; i < 3; i++)
+            // Выводим результат проверки
+            if (areEqual)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (matritsa1[i, j] == matritsa2[i, j])
-                    {
-                        // Выделяем совпадающие элементы
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(matritsa1[i, j] + " ");
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        // Выводим разные элементы
-                        Console.Write($"{matritsa1[i, j]} {matritsa2[i, j]} ");
-                    }
-                }
-                Console.WriteLine();
+                Console.WriteLine("\nМатрицы равны.");
             }
-            Console.ResetColor();
+            else
+            {
+                Console.WriteLine("\nМатрицы не равны. Выводим элементы с индикацией равных элементов:");
+
+                // Выводим обе матрицы с индикацией равных элементов
+                Console.WriteLine("\nПервая матрица:");
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (matritsa1[i, j] == matritsa2[i, j])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green; // Зеленый для равных элементов
+                            Console.Write(matritsa1[i, j] + " ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write(matritsa1[i, j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\nВторая матрица:");
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (matritsa1[i, j] == matritsa2[i, j])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green; // Зеленый для равных элементов
+                            Console.Write(matritsa2[i, j] + " ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write(matritsa2[i, j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
-}
-        
-    
+} 
 
-    
+
+
+
+
+
 
