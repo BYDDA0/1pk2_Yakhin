3ed26df98d7d3699b6c6522e9d5da5242c6d6c77
BYDDA0 <renatyahin49@gmail.com>
2025-02-01 23:38:24 +0500

    Выполнено 6 задача 5 урока

--- Task_05_06/Program.cs
+++ Task_05_06/Program.cs
@@ -12,6 +12,10 @@ namespace Task_05_06
       *Осуществить переворот массива (поменять строки и столбцы местами) вывести обновленный массив */
         static void Main(string[] args)
         {
+            // Определяем размеры массива
+            int desat = 10;
+            int pat = 5;
+
             // Создаем двумерный массив размером 10x5
             int[,] array = new int[10, 5];
 
@@ -24,47 +28,40 @@ namespace Task_05_06
                 array[i, 3] = (i + 1) * 4; // 4 столбец содержит числа кратные 4
                 array[i, 4] = (i + 1) * 5; // 5 столбец содержит числа кратные 5
             }
-            // Выводим исходный массив на панель
+            // Выводим исходный массив
             Console.WriteLine("Исходный массив:");
-            PrintArray(array);
-
-            // Транспонируем массив
-            int[,] transposedArray = TransposeArray(array);
-
-            // Выводим обновленный массив на панель
-            Console.WriteLine("Транспонированный массив:");
-            PrintArray(transposedArray);
-        }
-
-        static void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < desat; i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < pat; j++)
                 {
-                    Console.Write(array[i, j].ToString().PadLeft(4)); // Форматируем вывод 
+                    Console.Write(array[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
-        }
 
-        static int[,] TransposeArray(int[,] array)
-        {
-            int naprav1 = array.GetLength(0);
-            int naprav2 = array.GetLength(1);
-            int[,] transposed = new int[naprav2, naprav1];
+            // Создаем новый массив для транспонирования
+            int[,] transposedArray = new int[pat, desat];
 
-            for (int i = 0; i < naprav1; i++)
+            // Выполняем транспонирование массива
+            for (int i = 0; i < desat; i++)
             {
-                for (int j = 0; j < naprav2; j++)
+                for (int j = 0; j < pat; j++)
                 {
-                    transposed[j, i] = array[i, j]; // Меняем местами строки и столбцы
+                    transposedArray[j, i] = array[i, j];
                 }
             }
 
-            return transposed;
+            // Выводим транспонированный массив
+            Console.WriteLine("\nТранспонированный массив:");
+            for (int i = 0; i < pat; i++)
+            {
+                for (int j = 0; j < desat; j++)
+                {
+                    Console.Write(transposedArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
         }
-          
     }
 }
