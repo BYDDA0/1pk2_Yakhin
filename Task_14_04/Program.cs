b980ba6e04aadc6dc0efb3608b9d17e7292a47d5
BYDDA0 <renatyahin49@gmail.com>
2025-03-19 20:20:33 +0500

    Выполнена 4 задача 14 урока

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_14_04
+{
+    internal class Program
+    {/* Определите класс User, который будет иметь статическое свойство CurrentUser,
+      * представляющее текущего пользователя, 
+      * и метод для его установки */
+        static void Main(string[] args)
+        {
+            // Создаем  пользователей
+           
+            User user1 = new User("Егор Петросянов");
+            User user2 = new User("Сергей Булкин");
+            User user3 = new User("Петя Пирожков");
+           
+            // Вывод пользователей
+            User.SetCurrentUser(user1);
+            Console.WriteLine($"Текущий пользователь: {User.CurrentUser.Name}");
+
+            
+            User.SetCurrentUser(user2);
+            Console.WriteLine($"Текущий пользователь: {User.CurrentUser.Name}");
+            
+            User.SetCurrentUser(user3);
+            Console.WriteLine($"Текущий пользователь: {User.CurrentUser.Name}");
+        }
+    }
+}
---
+++ Task_14_04/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_04
+{
+    public class User
+    {
+        // Статическое свойство для получения
+        private static User _currentUser;
+
+        public string Name { get; set; } // Имя пользователя
+
+        // Статическое свойство для возврата
+        public static User CurrentUser
+        {
+            get { return _currentUser; }
+        }
+
+        // Метод для установки 
+        public static void SetCurrentUser(User user)
+        {
+            _currentUser = user;
+        }
+
+        // Конструктор
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
