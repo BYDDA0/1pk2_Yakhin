4963c0a91eeb41b36e406b285a2205704c134f25
BYDDA0 <renatyahin49@gmail.com>
2025-03-20 20:13:30 +0500

    Выполнена 1 задача 11 урока

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace Task_11_01
+{ /* Передача по значению:
+   * Напишите метод, который принимает два целых числа
+   * и меняет их местами.
+   * Проверьте, изменились ли значения переменных вне метода */
+    internal class Program
+    {
+        static void Swap(int a, int b)
+        {
+           int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            Console.WriteLine($"до обмена мест: x= {x}, y = {y}");
+            Swap(x, y);
+            Console.WriteLine($"После обмена мест: x= {y}, y = {x}");
+        }
+    }
+}
