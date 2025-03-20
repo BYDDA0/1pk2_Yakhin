ebb632878d071eb8fcc1f54c1ec9123be4ab7449
BYDDA0 <renatyahin49@gmail.com>
2025-03-20 20:14:04 +0500

    Выполнена 5 задача 11 урока

---
+++ Task_11_05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_11_05
+{
+    internal class Program
+    { /* Комбинирование ref и out: 
+       * Напишите метод, который принимает два числа по ссылке (ref)
+       * и возвращает их сумму и произведение через выходные параметры (out).*/
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 100;
+            int summResult, productResult;
+            SummAndProduct(ref x, ref y, out summResult, out productResult);
+            Console.WriteLine($"Сумма: {summResult}");
+            Console.WriteLine($"Произведение: {productResult}");
+        }
+        static void SummAndProduct( ref int a, ref int b, out int summ, out int product)
+        {
+            summ = a + b; // вычисление суммы 
+            product = a * b; // вычисление произведения
+        }
+    }
+}
