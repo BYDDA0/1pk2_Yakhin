f022798e024632eeded11c343c317503bd074166
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:42:08 +0500

    Выполнена 5 задача 20 урока

---
+++ Task_20_05/AuthorizationSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_05
+{
+    enum AccessLevel
+    {
+        Guest,      // Только чтение
+        User,       // Чтение + комментарии
+        Moderator,  // Удаление контента Чтение + комментарии
+        Admin       // Полный доступ
+    }
+
+    class AuthorizationSystem
+    {
+        public static void CheckPermission(AccessLevel userLevel, string action)
+        {
+            bool hasAccess = false;
+            string requiredLevel = "";
+
+            // Настройка доступа для пользователей
+            switch (action.ToLower())
+            {
+                case "read":
+                    hasAccess = true; // Чтение (гость и все остальные пользователи)
+                    requiredLevel = "Guest+";
+                    break;
+
+                case "comment":
+                    hasAccess = userLevel >= AccessLevel.User; // Комент (пользователь и выше)
+                    requiredLevel = "User+";
+                    break;
+
+                case "delete":
+                    hasAccess = userLevel >= AccessLevel.Moderator; // Удаление (модератор и выше)
+                    requiredLevel = "Moderator+";
+                    break;
+
+                case "admin":
+                    hasAccess = userLevel == AccessLevel.Admin; // админка (админ и выше)
+                    requiredLevel = "Admin";
+                    break;
+
+                default:
+                    Console.WriteLine($"Неизвестное действие: {action}");
+                    return;
+            }
+
+            if (hasAccess)
+            {                
+                Console.WriteLine($"Доступ разрешен: {userLevel} может выполнить '{action}'");       
+            }
+            else
+            {                
+                Console.WriteLine($"Ошибка: Недостаточно прав! ({userLevel} < {requiredLevel})");                
+            }
+        }
+    }
+}
---
+++ Task_20_05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_20_05
+{
+    internal class Program
+    { /* Напишите программу, имитирующую систему авторизации с использованием перечисления AccessLevel:
+       * Guest (только чтение)
+       * User (чтение + комментарии)
+       * Moderator (удаление контента)
+       * Admin (полный доступ)
+       * Создайте метод, который проверяет, может ли пользователь выполнить действие (например, удалитьпост).
+       * На основе уровня доступа выводите сообщение (например, "Ошибка: Недостаточно прав!")*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестирование системы авторизации\n");
+
+            // Проверка всех пользователей 
+            TestAccess(AccessLevel.Guest);
+            TestAccess(AccessLevel.User);
+            TestAccess(AccessLevel.Moderator);
+            TestAccess(AccessLevel.Admin);
+        }
+
+        static void TestAccess(AccessLevel level)
+        {
+            Console.WriteLine($"\n=== Тест для уровня: {level} ===");
+            AuthorizationSystem.CheckPermission(level, "read");
+            AuthorizationSystem.CheckPermission(level, "comment");
+            AuthorizationSystem.CheckPermission(level, "delete");
+            AuthorizationSystem.CheckPermission(level, "admin");
+        }
+    }
+}
