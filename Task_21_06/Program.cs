73c0963140e2e8575dec06a0669ea2685e07cd3d
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:43:22 +0500

    Выполнена 6 задача 21 урока

---
+++ Task_21_06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task_21_06
+{
+    internal class Program
+    { /* Написать метод, который удаляет повторяющиеся элементы из списка, 
+       * сохраняя порядок. */
+#pragma warning disable
+        static List<number> RemoveDuplicates<number>(List<number> list)
+        {
+            var result = new List<number>();
+            foreach (var i in list)
+            {
+                if (!result.Contains(i))
+                    result.Add(i);
+            }
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 2, 4, 1, 5, 4, 1, 6, 7};
+            var uniqueNumbers = RemoveDuplicates(numbers);
+
+            Console.WriteLine("Исходный список: " + string.Join(", ", numbers));
+            Console.WriteLine("Список без повторений: " + string.Join(", ", uniqueNumbers));
+        }
+    }
+}
