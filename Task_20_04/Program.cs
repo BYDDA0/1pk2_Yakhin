11836fff59ce150d696e8ff7c2c0b8e1ea1a6955
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:41:50 +0500

    Выполнена 4 задача 20 урока

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_20_04
+{
+    internal class Program
+    { /* Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+           * Car
+           * Bike
+           * Bus
+           * Truck
+           * Motorcycle
+           * Храните список транспортных средств (можно просто List<VehicleType>).
+           * Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков). 
+           * Реализуйте поиск по типу и вывод информации.*/
+        static void Main(string[] args)
+        { 
+            Vehicle tracker = new Vehicle();
+
+            // Добавление транспорта
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Truck);
+            tracker.AddVehicle(VehicleType.Bike);
+            tracker.AddVehicle(VehicleType.Bike);
+            tracker.AddVehicle(VehicleType.Car);
+            tracker.AddVehicle(VehicleType.Bus);
+            tracker.AddVehicle(VehicleType.Bus);
+            tracker.AddVehicle(VehicleType.Bus);
+            tracker.AddVehicle(VehicleType.Truck);
+            tracker.AddVehicle(VehicleType.Truck);
+
+            // Поиск грузовиков
+            var trucks = tracker.FindVehicles(VehicleType.Truck);
+            Console.WriteLine("\nКоличество грузовиков:");
+            foreach (var truck in trucks)
+            {
+                Console.WriteLine(truck);
+            }
+
+            // Подсчет автомобилей
+            int carCount = tracker.CountVehicles(VehicleType.Car);
+            Console.WriteLine($"\nВсего авто: {carCount}");
+
+            // Вывод 
+            tracker.PrintStatistics();
+        }
+    }
+}
+    
+
---
+++ Task_20_04/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_04
+{
+    enum VehicleType
+    {
+        Car,
+        Bike,
+        Bus,
+        Truck,
+        Motorcycle
+    }
+    internal class Vehicle
+    {
+        private List<VehicleType> vehicles = new List<VehicleType>();
+        
+        // Добавление транспортного средства
+        public void AddVehicle(VehicleType type)
+        {
+            vehicles.Add(type);
+            Console.WriteLine($"Добавлено: {type}");
+        }
+
+        // Подсчет транспорта определенного типа
+        public int CountVehicles(VehicleType type)
+        {
+            return vehicles.Count(v => v == type);
+        }
+
+        // Поиск транспорта по типу
+        public List<VehicleType> FindVehicles(VehicleType type)
+        {
+            return vehicles.Where(v => v == type).ToList();
+        }
+
+        // Вывод всей статистики
+        public void PrintStatistics()
+        {
+            Console.WriteLine("\nСтатистика транспорта:");
+            foreach (VehicleType type in Enum.GetValues(typeof(VehicleType)))
+            {
+                Console.WriteLine($"{type}: {CountVehicles(type)} шт.");
+            }
+        }
+
+    }
+}
