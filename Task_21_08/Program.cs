8ba4074c8da6d5dbce049baf4f842aba84260f45
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:45:08 +0500

    Выполнена 8 задача 21 урока

---
+++ Task_21_08/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    { /* напишите метод, который на вход получается массив параметров (строк)
+       * и возвращает только уникальные строки*/
+        static void Main(string[] args)
+        {
+            string[] eat = { "яблоко", "груша", "яблоко", "апельсин", "банан" };
+
+            string[] array = GetUniqueStrings(eat);
+
+            Console.WriteLine("Исходный массив: " + string.Join(", ", eat));
+            Console.WriteLine("Уникальные строки: " + string.Join(", ", array));
+        }
+
+        // Метод для уникальных строк
+        static string[] GetUniqueStrings(string[] inputArray)
+        {
+            if (inputArray == null || inputArray.Length == 0)
+                return Array.Empty<string>();
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool Duplicate = false;
+
+                // Проверка, встречалась ли слово ранее в массиве
+                for (int j = 0; j < i; j++)
+                {
+                    if (inputArray[i] == inputArray[j])
+                    {
+                        Duplicate = true;
+                        break;
+                    }
+                }
+
+                if (!Duplicate)
+                {
+                    result.Add(inputArray[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
