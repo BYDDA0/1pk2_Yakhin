6806d05bd7e408b086ebdb0db2a3ccf551949b4d
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:40:43 +0500

    Выполнена 3 задача 19 урока

---
+++ Task_19_03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_19_03
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            string line;
+            Console.WriteLine("Введите строку:" + 
+                "\n(Для завершения ввода введите пустую строку)");
+            while (true)
+            {
+                line = Console.ReadLine();
+                // проверка на не пустую строку 
+                if (String.IsNullOrWhiteSpace(line)) 
+                {
+                    break;
+                }
+                // добавляем "-" после каждого слова
+                if (result.Length > 0) 
+                {
+                    result += "-";
+                }
+                result += line;
+                Console.WriteLine($"Результат: \"{result}\"");
+            }
+        }
+    }
+}
