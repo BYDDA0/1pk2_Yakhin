f3839765e973f1265632d64f893bb645a3f35b0b
BYDDA0 <renatyahin49@gmail.com>
2025-04-11 19:44:50 +0500

    Выполнена 7 задача 21 урока

---
+++ Task_21_07/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_21_07
+{
+    internal class Program
+    { /* Написать метод, который находит первый ключ в словаре, соответствующий заданному значению.
+       * Если значения нет, вернуть null.*/
+
+#pragma warning disable
+
+        static void Main(string[] args)
+        {
+            // Создаем словарь
+            var dict = new Dictionary<string, int>
+        {
+            {"яблоко", 1},
+            {"банан", 2},
+            {"апельсин", 3},
+            {"груша", 2}  // Дубликат значения
+        };
+
+            // Ищем ключ для значения 2
+            string key = FindKey(dict, 2);
+            Console.WriteLine(key ?? "Не найдено"); // Выведет "банан"
+
+            // Ищем несуществующее значение
+            key = FindKey(dict, 5);
+            Console.WriteLine(key ?? "Не найдено"); // Выведет "Не найдено"
+        }
+
+        // Метод поиска ключа 
+        static TKey FindKey<TKey, TValue>(Dictionary<TKey, TValue> dict, TValue value)
+        {
+            foreach (var item in dict)
+            {
+                if (item.Value.Equals(value))
+                    return item.Key;
+            }
+            return default; // Вернет null нет значений
+        }
+    }
+}
